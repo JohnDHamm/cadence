66b3943dbd75f1f50bb1c13ba8990b4fdb1fe937
John Hamm <johndhamm1@gmail.com>
2017-04-20 15:43:42 -0500

    athlete + results list styled

--- app/controllers/CounterCtrl.js
+++ app/controllers/CounterCtrl.js
@@ -3,19 +3,22 @@
 app.controller("CounterCtrl", function($scope, CounterFactory, $location){
 
 	const counterDiv = document.getElementById('counterDiv');
+	const groupBtnDiv = document.getElementById('groupDiv');
+	const resultsBtnDiv = document.getElementById('resultsDiv');
+	const listBGDiv = document.getElementById('listBG');
 
-	$scope.athletes = [{id: 0, name: "Clara", result: 0.00},
-											{id: 1, name: "Lucy", result: 0.00},
-											{id: 2, name: "Makenna", result: 0.00},
-											{id: 3, name: "Maya", result: 0.00},
-											{id: 4, name: "Miles", result: 0.00},
-											{id: 5, name: "Trinity", result: 0.00},
-											{id: 6, name: "Ruth", result: 0.00},
-											{id: 7, name: "Wellington", result: 0.00}];
-	$scope.showAthleteList = true;
+	$scope.athletes = [{id: 0, name: "CLARA", result: 0.00},
+											{id: 1, name: "LUCY", result: 0.00},
+											{id: 2, name: "MAKENNA", result: 0.00},
+											{id: 3, name: "MAYA", result: 0.00},
+											{id: 4, name: "MILES", result: 0.00},
+											{id: 5, name: "TRINITY", result: 0.00},
+											{id: 6, name: "RUTH", result: 0.00},
+											{id: 7, name: "WELLINGTON", result: 0.00}];
+	$scope.showAthleteList = false;
 	$scope.showResultList = false;
 
-	$scope.currentAthlete = {name: ""};
+	$scope.currentAthlete = $scope.athletes[0];
 	$scope.avgSecond = 0.00;
 	$scope.avgRPM = 0;
 
@@ -55,15 +58,26 @@ app.controller("CounterCtrl", function($scope, CounterFactory, $location){
 
 	$scope.showAthletes = () => {
 		counterDiv.classList.remove('animCounter');
+		listBGDiv.style.background = "rgba(55, 0, 0, 0.8)"
+		// groupBtnDiv.classList.add('btnSelected');
+		// resultsBtnDiv.classList.remove('btnSelected');
+		listBGDiv.style.display = "block";
 		$scope.showResultList = false;
 		$scope.showAthleteList = true;
 	}
 
 	$scope.selectAthlete = (index) => {
+		// groupBtnDiv.classList.remove('btnSelected');
+		listBGDiv.style.display = "none";
 		clearCounter(index);
 		$scope.currentAthlete = $scope.athletes[index];
 		$scope.avgSecond = $scope.athletes[index].result.toFixed(2);
 		$scope.showAthleteList = false;
+	}
+
+	$scope.resetCounter = () => {
+		counterDiv.classList.remove('animCounter');
+		clearCounter($scope.currentAthlete.id);
 	}
 
 	const clearCounter = (index) => {
@@ -76,10 +90,19 @@ app.controller("CounterCtrl", function($scope, CounterFactory, $location){
 
 	$scope.showResults = () => {
 		counterDiv.classList.remove('animCounter');
-		$scope.showResultList ?
-			($scope.showAthleteList = true,
-				$scope.showResultList = false) : $scope.showResultList = true;
-		// $scope.showResultList = !$scope.showResultList;
+		$scope.showResultList ? $scope.showAthletes()
+			:
+			($scope.showResultList = true,
+				$scope.showAthleteList = false,
+				listBGDiv.style.display = "block",
+				listBGDiv.style.background = "rgba(0, 0, 55, 0.8)"
+				);
+	}
+
+	$scope.clearAllResults = () => {
+		for (let i = 0; i < $scope.athletes.length; i++){
+			$scope.athletes[i].result = 0;
+		}
 	}
 
 
