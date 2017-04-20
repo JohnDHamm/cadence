eaa8839ed6139c3295514469fee29343c2cc5a60
John Hamm <johndhamm1@gmail.com>
2017-04-20 00:02:11 -0500

    athlete list started

--- app/controllers/CounterCtrl.js
+++ app/controllers/CounterCtrl.js
@@ -4,33 +4,32 @@ app.controller("CounterCtrl", function($scope, CounterFactory, $location){
 
 	const readout = document.getElementById('readoutText');
 	const counterDiv = document.getElementById('counterDiv');
-	console.log("counterDiv", counterDiv);
 
+	$scope.athletes = ["Clara", "Lucy", "Makenna", "Maya", "Miles", "Trinity", "Wellington"];
 	$scope.currentAthlete = "Wellington";
+	$scope.showAthleteList = false;
 	$scope.avgSecond = '0.00';
+	$scope.avgRPM = 0;
 	let prevTap = 0,
 			latestTap = 0,
 			intervals = [];
-	$scope.avgRPM = 0;
 
 	$scope.clicked = () => {
 		counterDiv.classList.add('animCounter');
-		console.log("counterDiv", counterDiv);
 
 		let thisTap = CounterFactory.getTime();
+
 		if (prevTap === 0) {
 			prevTap = thisTap;
 			latestTap = thisTap;
 		} else {
 			prevTap = latestTap;
 			latestTap = thisTap;
-
 			let thisInterval = ((latestTap - prevTap)/1000);
 			intervals.push(thisInterval);
 			const averages = calcAverages();
 			$scope.avgSecond = averages.avgSecond;
 			$scope.avgRPM = averages.avgRPM;
-
 		}
 	}
 
@@ -40,6 +39,15 @@ app.controller("CounterCtrl", function($scope, CounterFactory, $location){
 		return { avgSecond: (sum / intervals.length).toFixed(2),
 						avgRPM: Math.floor(60 / (sum / intervals.length))
 					};
+	}
+
+	$scope.showAthletes = () => {
+		$scope.showAthleteList = true;
+		console.log("clicked group");
+	}
+
+	$scope.selectAthlete = (index) => {
+		console.log("index", index);
 	}
 
 
